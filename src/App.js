015e670e5e90fa3d271469e41d3876fd82d30e6f
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-10-15 23:46:55 +0100

    Added graphql server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import logo from "./spacex.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <img
+        src={logo}
+        alt=""
+        style={{ width: 200, display: "block", margin: "auto" }}
+      />
     </div>
   );
 }
