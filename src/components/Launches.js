2e280e0e7ade1ecd33b2882f7b10224334b43f71
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-10-18 20:57:56 +0100

    Launches file setup

--- src/components/Launches.js
+++ src/components/Launches.js
@@ -4,11 +4,16 @@ import { useQuery, gql } from "@apollo/client";
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
     launches {
-      flight_number
       name
-      date_local
-      success
       id
+      flight_number
+      success
+      date_local
+      cores {
+        flight
+        core
+        landpad
+      }
     }
   }
 `;
@@ -17,16 +22,18 @@ function Launches() {
   const { loading, error, data } = useQuery(LAUNCHES_QUERY);
   if (loading) return <p>Loading...</p>;
   if (error) return console.log(error);
-  return data.map(({ id, flight_number, name, date_local, success }) => (
-    <div key={id}>
-      <h1 className="display-4 my-3">Launches</h1>
-      <h3>{name}</h3>
-      <br />
-      <b>About this location:</b>
-      <p>{flight_number}</p>
-      <br />
-    </div>
-  ));
+  return data.launches.map(
+    ({ id, flight_number, name, date_local, success }) => (
+      <div key={id}>
+        <h1 className="display-4 my-3">Launches</h1>
+        <h3>{name}</h3>
+        <br />
+        <b>About this location:</b>
+        <p>{flight_number}</p>
+        <br />
+      </div>
+    )
+  );
 }
 
 export default Launches;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  from,
+  HttpLink,
+} from "@apollo/client";
 import "./index.css";
 import App from "./App";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      return alert(`Graphql errors ${message}`);
+    });
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000/graphql",
+});
 
 const client = new ApolloClient({
-  uri: "http:localhost:5000/graphql",
   cache: new InMemoryCache(),
+  link: from([errorLink, httpLink]),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
