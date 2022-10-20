c5288be7cfe2bb70bbdafdd3665244dbe02cb757
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-10-20 14:06:30 +0100

    Added the LaunchItem file

--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
+import LaunchItem from "./LaunchItem";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -20,19 +21,16 @@ const LAUNCHES_QUERY = gql`
 
 function Launches() {
   const { loading, error, data } = useQuery(LAUNCHES_QUERY);
-  if (loading) return <p>Loading...</p>;
-  if (error) return console.log(error);
-  return data.launches.map(
-    ({ id, flight_number, name, date_local, success }) => (
-      <div key={id}>
-        <h1 className="display-4 my-3">Launches</h1>
-        <h3>{name}</h3>
-        <br />
-        <b>About this location:</b>
-        <p>{flight_number}</p>
-        <br />
-      </div>
-    )
+  return (
+    <>
+      <h1 className="display-4 my-3">Launches</h1>
+      {loading && <h4>Loading...</h4>}
+      <>
+        {data.launches.map((launch) => (
+          <LaunchItem key={launch.flight_number} launch={launch} />
+        ))}
+      </>
+    </>
   );
 }
 
