c9d3e431f8717a16fc67f451dfd9cde6bb52e280
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-11-23 19:28:13 +0100

    feat: Added necessary packages like Router, classNames and Moment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
 import logo from "./spacex.jpg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Launches from "./components/Launches";
+import MissionKey from "./components/MissionKey";
 
 function App() {
   return (
-    <div className="container">
-      <img
-        src={logo}
-        alt="SpaceX"
-        style={{ width: 200, display: "block", margin: "auto" }}
-      />
-      <h1 className="display-4 my-3">Launches</h1>
-      <Launches />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <img
+          src={logo}
+          alt="SpaceX"
+          style={{ width: 200, display: "block", margin: "auto" }}
+        />
+        <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey />
+        <Routes>
+          <Route path="/" component={<Launches />} />
+        </Routes>
+        <Launches />
+      </div>
+    </BrowserRouter>
   );
 }
 
