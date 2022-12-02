56d80526969d4ed3156f0bbdc93d8e965aff4964
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-12-02 17:12:09 +0100

    feat: Completed App

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Moment from "react-moment";
+import classNames from "classnames";
 
 const LAUNCH_QUERY = gql`
   query LaunchQuery($id: String!) {
@@ -25,14 +27,52 @@ function Launch() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
+  console.log(data);
 
-  const { name, flight_number, success, date_local } = data.launch;
+  const {
+    name,
+    flight_number,
+    success,
+    date_local,
+    cores: {
+      0: { core },
+    },
+  } = data.launch;
 
   return (
     <div>
       <h1 className="display-4 my-3">
         <span className="text-dark">Mission:</span> {name}
       </h1>
+      <h4 className="mb-3">Launch Details</h4>
+      <ul className="list-group">
+        <li className="list-group-item">Flight Number: {flight_number}</li>
+        <li className="list-group-item">
+          Launch year: <Moment format="YYYY">{date_local}</Moment>
+        </li>
+        <li className="list-group-item">
+          Launch Successful:{" "}
+          <span
+            className={classNames({
+              "text-success": success,
+              "text-danger": !success,
+            })}
+          >
+            {success ? "Yes" : "No"}
+          </span>
+        </li>
+      </ul>
+      <h4 className="my-3">Rocket Details</h4>
+      <ul className="list-group">
+        <li className="list-group-item">Rocket ID: {core}</li>
+        <li className="list-group-item">
+          Rocket Name: {name} {flight_number}
+        </li>
+      </ul>
+      <hr />
+      <Link to="/" className="btn btn-secondary">
+        Back
+      </Link>
     </div>
   );
 }
